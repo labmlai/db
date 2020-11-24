f6be48f8c724bf76fc6dc966cb4e13bd6501e911
Varuna Jayasiri <vpjayasiri@gmail.com>
2020-11-24 08:56:13 +0530

    comemnts

--- test/redis/__init__.py
+++ test/redis/__init__.py
@@ -4,4 +4,6 @@ Client: https://github.com/andymccurdy/redis-py
 
 * Clear all dbs
     ./redis-cli flushall
+* Backup db
+    https://www.digitalocean.com/community/tutorials/how-to-back-up-and-restore-your-redis-data-on-ubuntu-14-04
 """
--- test/redis/test.py
+++ test/redis/test.py
@@ -1,4 +1,3 @@
-from pathlib import Path
 from typing import List, Optional
 
 import redis
