f055e599da3c54acf510fc70268e9e66e023fc37
hnipun <hnipun@gmail.com>
2021-04-16 10:42:52 +0530

    read data and save from model_key

--- labml_db/model.py
+++ labml_db/model.py
@@ -64,6 +64,12 @@ class Key(Generic[_KT]):
     def delete(self):
         return Model.delete_by_key(self._key)
 
+    def save(self, data: ModelDict):
+        return Model.save_by_key(self._key, data)
+
+    def read(self):
+        return Model.read_dict(self._key)
+
     def __repr__(self):
         return f'Key({self._key})'
 
@@ -191,6 +197,14 @@ class Model(Generic[_KT]):
         Model.__db_drivers = {d.model_name: d for d in db_drivers}
 
     @classmethod
+    def read_dict(cls, key: str) -> ModelDict:
+        model_name = key.split(':')[0]
+        db_driver = Model.__db_drivers[model_name]
+        data = db_driver.load_dict(key)
+
+        return data
+
+    @classmethod
     def load(cls, key: str) -> Optional[_KT]:
         model_name = key.split(':')[0]
         db_driver = Model.__db_drivers[model_name]
@@ -204,6 +218,12 @@ class Model(Generic[_KT]):
         model_name = key.split(':')[0]
         db_driver = Model.__db_drivers[model_name]
         db_driver.delete(key)
+
+    @staticmethod
+    def save_by_key(key: str, data: ModelDict):
+        model_name = key.split(':')[0]
+        db_driver = Model.__db_drivers[model_name]
+        db_driver.save_dict(key, data)
 
     def to_dict(self) -> ModelDict:
         values = {}
