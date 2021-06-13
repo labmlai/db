c64eb4b4da9c4f73510e69e64e4268b3833f20ec
Varuna Jayasiri <vpjayasiri@gmail.com>
2021-06-13 09:29:05 +0530

    mget index

--- labml_db/index_driver/redis.py
+++ labml_db/index_driver/redis.py
@@ -1,4 +1,4 @@
-from typing import Dict, Type, Optional, TYPE_CHECKING
+from typing import Dict, Type, Optional, TYPE_CHECKING, List
 
 from . import IndexDbDriver
 
@@ -23,6 +23,10 @@ class RedisIndexDbDriver(IndexDbDriver):
 
     def get(self, index_key: str) -> str:
         return self._db.hget(self._index_key, index_key)
+
+    def mget(self, index_key: List[str]) -> List[str]:
+        return self._db.hmget(self._index_key, index_key)
+
 
     def set(self, index_key: str, model_key: str):
         self._db.hset(self._index_key, index_key, model_key)
